21192f44a4078b9cb11a4a0dd97659caf19176e1
Marc Alvarez <marcalvarez@fastmail.com>
2025-05-18 22:05:03 -0600

    [gitbak] Automatic checkpoint #87 - 2025-05-18 22:05:03

--- internal/setup/interface.go
+++ internal/setup/interface.go
@@ -1,40 +1,62 @@
 package setup
 
 import (
+	"fmt"
 	"github.com/bashhack/sesh/internal/keychain"
 )
 
-// WizardRunner defines an interface for running the setup wizard
-type WizardRunner interface {
-	Run() error
-	RunForService(serviceName string) error
+// SetupHandler defines a handler for a specific service setup
+type SetupHandler interface {
+	ServiceName() string
+	Setup() error
 }
 
-// DefaultWizardRunner is the default implementation of WizardRunner
-type DefaultWizardRunner struct {
-	KeychainProvider keychain.Provider
+// SetupService is the main service for setting up credentials
+type SetupService interface {
+	// RegisterHandler registers a setup handler for a service
+	RegisterHandler(handler SetupHandler)
+	
+	// SetupService initiates the setup process for a specific service
+	SetupService(serviceName string) error
+	
+	// GetAvailableServices returns a list of services that can be set up
+	GetAvailableServices() []string
 }
 
-// NewDefaultWizardRunner creates a new DefaultWizardRunner with the given keychain provider
-func NewDefaultWizardRunner(provider keychain.Provider) *DefaultWizardRunner {
-	return &DefaultWizardRunner{
-		KeychainProvider: provider,
+// setupServiceImpl is the implementation of SetupService
+type setupServiceImpl struct {
+	handlers         map[string]SetupHandler
+	keychainProvider keychain.Provider
+}
+
+// NewSetupService creates a new SetupService
+func NewSetupService(provider keychain.Provider) SetupService {
+	return &setupServiceImpl{
+		handlers:         make(map[string]SetupHandler),
+		keychainProvider: provider,
 	}
 }
 
-// Run implements WizardRunner interface
-func (w *DefaultWizardRunner) Run() error {
-	return w.RunForService("aws")
+// RegisterHandler registers a setup handler for a service
+func (s *setupServiceImpl) RegisterHandler(handler SetupHandler) {
+	s.handlers[handler.ServiceName()] = handler
 }
 
-// RunForService runs the setup wizard for a specific service
-func (w *DefaultWizardRunner) RunForService(serviceName string) error {
-	switch serviceName {
-	case "aws":
-		return setupAWSWithProvider(w.KeychainProvider)
-	case "totp":
-		return setupGenericTOTPWithProvider(w.KeychainProvider)
-	default:
-		return nil
+// SetupService initiates the setup process for a specific service
+func (s *setupServiceImpl) SetupService(serviceName string) error {
+	handler, exists := s.handlers[serviceName]
+	if !exists {
+		return fmt.Errorf("no setup handler registered for service: %s", serviceName)
 	}
+	
+	return handler.Setup()
+}
+
+// GetAvailableServices returns a list of services that can be set up
+func (s *setupServiceImpl) GetAvailableServices() []string {
+	services := make([]string, 0, len(s.handlers))
+	for service := range s.handlers {
+		services = append(services, service)
+	}
+	return services
 }
