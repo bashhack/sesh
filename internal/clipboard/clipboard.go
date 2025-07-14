d9ed2af9971ed0604cf86b92651a9d2f79d80ac3
Marc Alvarez <marcalvarez@fastmail.com>
2025-07-13 21:20:11 -0600

    Add clipboard targeted for OSX

---
+++ internal/clipboard/clipboard.go
@@ -0,0 +1,45 @@
+package clipboard
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+var (
+	execCommand = exec.Command
+	runtimeGOOS = runtime.GOOS
+)
+
+// Copy copies text to the clipboard and returns an error if unsuccessful
+func Copy(text string) error {
+	switch runtimeGOOS {
+	case "darwin":
+		return copyOSX(text)
+	default:
+		return fmt.Errorf("unsupported platform: %s", runtimeGOOS)
+	}
+}
+
+// copyOSX copies text to clipboard on macOS
+func copyOSX(text string) error {
+	cmd := execCommand("pbcopy")
+	pipe, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	if _, err := pipe.Write([]byte(text)); err != nil {
+		return err
+	}
+
+	if err := pipe.Close(); err != nil {
+		return err
+	}
+
+	return cmd.Wait()
+}
